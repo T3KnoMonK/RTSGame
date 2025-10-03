5f7b5194c909f9d99b3e31ad0199825840a379c3
Zachary Hebbel <zhebbel@gmail.com>
2025-10-02 21:01:52 -0700

    Added Minimaphelper.cs; Working on drawing a box on the minimap to show currently viewed space.

---
+++ Assets/Scripts/Game/MinimapHelper.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapHelper : MonoBehaviour
+{
+    public delegate void RegisterWithMinimapDelegate(GameObject mapObj);
+    public static RegisterWithMinimapDelegate RegisterWithMinimap;
+
+    private List<GameObject> _MapObjects = new List<GameObject>();
+
+    private Vector2 _MapSize;
+
+    private void OnEnable()
+    {
+        RegisterWithMinimap += AddMapObject;
+    }
+
+    private void OnDisable()
+    {
+        RegisterWithMinimap -= AddMapObject;
+    }
+
+    private void AddMapObject(GameObject mapObj)
+    {
+        _MapObjects.Add(mapObj);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
