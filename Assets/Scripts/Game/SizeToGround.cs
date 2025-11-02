05ae3ac2b5eec9cd32360797954106735f454299
Zachary Hebbel <zhebbel@gmail.com>
2025-11-02 00:57:42 -0700

    Fixed MinimapCamera not positioning correctly if the ground was a mesh.

--- Assets/Scripts/Game/SizeToGround.cs
+++ Assets/Scripts/Game/SizeToGround.cs
@@ -17,16 +17,17 @@ public class SizeToGround : MonoBehaviour
         else if (_Ground.GetComponent<MeshFilter>())
         {
             _GroundSize = _Ground.GetComponent<MeshFilter>().mesh.bounds.extents;
+            Vector3 scaledGround = new Vector3(_GroundSize.x * _Ground.transform.localScale.x, _GroundSize.y, _GroundSize.z * _Ground.transform.localScale.z);
+            _GroundSize = scaledGround;
         }
-            //Orthographic camera size is from center to extents so 
-            //Terrain bounds are (width, hight, length) from center; i.e. (500, 12.5, 500);
-            _CameraSize = (int)(_GroundSize.x > _GroundSize.z ? _GroundSize.x : _GroundSize.z);
+
+        //Orthographic camera size is from center to extents so 
+        //Terrain bounds are (width, hight, length) from center; i.e. (500, 12.5, 500);
+        _CameraSize = (int)(_GroundSize.x > _GroundSize.z ? _GroundSize.x : _GroundSize.z);
         //Take the largest extent so the minimap always fits inside the camera
+
         _MinimapCamera.orthographicSize = _GroundSize.x;
-        _MinimapCamera.transform.SetPositionAndRotation(
-            new Vector3
-                (_CameraSize, _MinimapCamera.transform.position.y, _CameraSize), 
-                _MinimapCamera.transform.rotation);
+        _MinimapCamera.transform.SetPositionAndRotation(new Vector3 (_CameraSize, _MinimapCamera.transform.position.y, _CameraSize), _MinimapCamera.transform.rotation);
     }
 
     private void Start()
